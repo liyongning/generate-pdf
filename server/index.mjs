2ae74c4e1d2a0b22dbd0f9cfc65ad1d95441f488
liyongning <liyongning@360.cn>
2023-12-20 17:18:21 +0800

    feat: 打印完整网页（网页滚动懒加载场景）

--- server/index.mjs
+++ server/index.mjs
@@ -10,6 +10,27 @@ async function generatePDF() {
   const page = await browser.newPage()
   // 在当前 Tab 页上打开 “百度新闻” 页。第二个配置参数，意思是当页面触发 load 事件，并且 500ms 内没有新的网络连接，则继续往下执行
   await page.goto('https://news.baidu.com', { waitUntil: ['load', 'networkidle0'] })
+  // 滚动页面，加载完整内容。evaluate 的回调函数会在浏览器中执行，evalaute 方法的返回值是回调函数的返回值
+  await page.evaluate(function () {
+    return new Promise(resolve => {
+      // 通过递归来滚动页面
+      function scrollPage() {
+        // { 浏览器窗口可视区域的高度，页面的总高度，已滚动的高度 }
+        const { clientHeight, scrollHeight, scrollTop } = document.documentElement
+        // 如果滚动高度 + 视口高度 < 总高度，则继续滚动，否则就任务滚动到底部了
+        if (scrollTop + clientHeight < scrollHeight) {
+          document.documentElement.scrollTo(0, scrollTop + clientHeight)
+          // 加一个 setTimeout 来保证滚动的稳定性
+          setTimeout(() => {
+            scrollPage()
+          }, 500)
+        } else {
+          resolve()
+        }
+      }
+      scrollPage()
+    })
+  })
   // 将当前页打印成 PDF 文件
   await page.pdf({
     // PDF 文件的存储路径，如果不设置则会以二进制的形式放到内存中
